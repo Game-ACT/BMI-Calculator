f849a06a860191ea9a3840ee9ee5393781f2669b
Game <thapat.g11@gmail.com>
2024-01-29 15:18:38 +0700

    add calc func

--- MyCalcView.swift
+++ MyCalcView.swift
@@ -7,15 +7,22 @@
 
 import SwiftUI
 
+// broken code requried fixing
 struct MyCalcView: View {
-    var body: some View {
-        Text("MyBMICalculatorView")
+    
+    // delare values
+    @State private var myWeight : Int = 55
+    @State private var myHeight : Int = 165
+    @State private var myBMIResult : String = ""
+    
+    func bmiresultinterpert(bmiValue: Double) -> String {
+        var result : String = "BMI Value = \(bmiValue) \n"
+        if bmiValue >= 40 {
+            result += "คุณเป๊นโรคอ้วนขั้นสูงสุด กรุณาพบแพทย์ด่วน"
+        } else if bmiValue >= 35 {
+            result += "คุณเป็นโรคอ้วนระดับ 2 คุณเสี่ยงต่อการเกิดโรคที่มากับความอ้วนหากคุณมีเส้นรอบเอวมากกว่าเกณท์ปกติคุณจะเสี่ยงต่อการเกิดโรคสูง คุณต้องควบคุมอาหาร และออกกำลังกายอย่างจริงจัง"
+        } else if bmiValue >= 28.5 {
+            result += "คุณเป็นโรคอ้วนระดับ 1 คุณจะมีโอกาสเกิดโรคความดันและเบาหวานจำเป็นต้องควบคุมอาหารและออกกำลังกาย"
+        }
     }
 }
-
-/*struct MyBMICalculatorView_Previews: PreviewProvider {
-    static var previews: some View {
-        MyCalcView()
-    }
-}
-*/
