57d62883671c2fc2b89a92eb42c572a1e5a4c22a
Game <thapat.g11@gmail.com>
2024-02-01 16:08:47 +0700

    change a bunch of stuff

--- MyCalcView.swift
+++ MyCalcView.swift
@@ -14,25 +14,32 @@ struct MyCalcView: View {
     @State private var myWeight : Int = 55
     @State private var myHeight : Int = 165
     @State private var myBMIResult : String = ""
+    @State private var myBMIInterpert : String = ""
+    
+    
+    func bmicalculate(bmiValue: Double) -> String {
+        var result : String = "BMI Value = \(round(bmiValue * 100) / 100) \n"
+        return result
+    }
     
     func bmiresultinterpert(bmiValue: Double) -> String {
-        var result : String = "BMI Value = \(round(bmiValue * 100) / 100) \n"
+        var result : String = ""
         // translation not final
         // (rant below)
         // it english cause the whole app is in english why would be in thai
         // and most people understand english so why not
         if bmiValue >= 40 {
-            result += "You are extremely obese. Please seek medical attention immediately."
+            result = "You are extremely obese. Please seek medical attention immediately."
         } else if bmiValue >= 35 {
-            result += "You are obese, level 2. You are at risk of developing obesity-related diseases. If your waist circumference is greater than normal, you are at higher risk of developing obesity. You must control your diet. and exercise seriously"
+            result = "You are obese, level 2. You are at risk of developing obesity-related diseases. If your waist circumference is greater than normal, you are at higher risk of developing obesity. You must control your diet. and exercise seriously"
         } else if bmiValue >= 28.5 {
-            result += "You are obese, level 1. You are more likely to develop high blood pressure and diabetes. You need to control your diet and exercise."
+            result = "You are obese, level 1. You are more likely to develop high blood pressure and diabetes. You need to control your diet and exercise."
         } else if bmiValue >= 23.5 {
-            result += "Overweight, If you are genetically affected by diabetes or high blood cholesterol, you must lose weight so that your body mass index is less than 23."
+            result = "Overweight, If you are genetically affected by diabetes or high blood cholesterol, you must lose weight so that your body mass index is less than 23."
         } else if bmiValue >= 18.5{
-            result += "Normal weight, If the amount of fat is within the normal range, there is rarely an incidence of diabetes and high blood pressure."
+            result = "Normal weight, If the amount of fat is within the normal range, there is rarely an incidence of diabetes and high blood pressure."
         } else {
-            result += "Underweight, You should eat high quality food. and has sufficient energy and exercise appropriately."
+            result = "Underweight, You should eat high quality food. and has sufficient energy and exercise appropriately."
         }
         return result
     }
@@ -74,7 +81,8 @@ struct MyCalcView: View {
                 HStack {
                     
                     Button(action: {
-                        self.myBMIResult = self.bmiresultinterpert(bmiValue: Double(self.myWeight) / (pow(Double(self.myHeight) / 100, 2)))
+                        self.myBMIResult = self.bmicalculate(bmiValue: Double(self.myWeight) / (pow(Double(self.myHeight) / 100, 2)))
+                        self.myBMIInterpert = self.bmiresultinterpert(bmiValue: Double(self.myWeight) / (pow(Double(self.myHeight) / 100, 2)))
                     }){
                         Text("Show BMI Value")
                             .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
@@ -85,6 +93,7 @@ struct MyCalcView: View {
                     
                     Button(action: {
                         self.myBMIResult = ""
+                        self.myBMIInterpert = ""
                     }){
                         Text("Clear Result")
                             .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
@@ -95,12 +104,19 @@ struct MyCalcView: View {
                 }
                 
                 .offset(y: 20)
+                
                 Text("\(myBMIResult)")
                     .lineLimit(nil)
                     .multilineTextAlignment(.leading)
+                    .font(.system(.headline, design: .rounded))
                     .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
-                    .offset(y : 25)
+                    .offset(y : 35)
                 
+                Text("\(myBMIInterpert)")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.leading)
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+                    .padding(.vertical, 15.0)
                 Spacer()
                 
             }
